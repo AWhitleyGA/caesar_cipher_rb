430c55539b5394deaa33eab05428508d08077416
Andrew Whitley <awhitley1233@gmail.com>
2016-09-28 09:57:55 -0400

    solved

--- starter/lib/cipher.rb
+++ starter/lib/cipher.rb
@@ -1,3 +1,18 @@
-def encode word
+require 'pry'
 
+
+def encode word
+  alphabet = ('a'..'z').to_a
+  letters = word.split('')
+  translatedArray = letters.map do |letter|
+    alphabet.index(letter) + 13 > 25? alphabet.index(letter) - 13 : alphabet.index(letter) + 13
+  end
+  translatedWord = translatedArray.map { |position| alphabet[position]}
+  return translatedWord.join
 end
+
+
+# word = gets.chomp
+# puts encode word
+
+binding.pry
